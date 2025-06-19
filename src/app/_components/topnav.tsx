6d74ef9baa30e596d2bd808e7d6741d2eaabca2d
XavierFabregat <xavi.fabregat.pous@gmail.com>
2025-06-19 17:31:14 +0200

    handle not authed logic for top nav

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -5,23 +5,31 @@ import SlashSeparator from "./slash-separator";
 import BusinessCombobox from "./busines-selector-combobox";
 import { getMyProjects } from "~/server/queries";
 import { cookies } from "next/headers";
+import { auth } from "@clerk/nextjs/server";
+import type { Project } from "../../types";
 
 export default async function TopNav() {
-  const projects = await getMyProjects();
-  const selectedProjectId = (await cookies()).get("selected_project")?.value;
-  const selectedProject = projects.find(
-    (proj) => proj.id === selectedProjectId,
-  );
+  const { userId } = await auth(); // ✅ Check auth context on server
 
+  let projects: Project[] = [];
+  let selectedProject = undefined;
+
+  if (userId) {
+    projects = await getMyProjects();
+    const selectedProjectId = (await cookies()).get("selected_project")?.value;
+    selectedProject = projects.find((proj) => proj.id === selectedProjectId);
+  }
   return (
     <nav className="bg-background border-foreground z-20 flex w-full items-center justify-between border-b p-4 px-10 text-xl font-semibold">
       <div className="flex items-center gap-2">
         <div>DelphiAI</div>
-        <SlashSeparator />
-        <BusinessCombobox
-          projects={projects}
-          selectedProject={selectedProject}
-        />
+        <SignedIn>
+          <SlashSeparator />
+          <BusinessCombobox
+            projects={projects}
+            selectedProject={selectedProject}
+          />
+        </SignedIn>
       </div>
       <div>
         <div className="flex flex-row items-center gap-4 px-5">
